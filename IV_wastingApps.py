de6ab10b1a82fa4f3de0ac35135ea9e66203e7bf
Theophile Niyitanga <theo@Theophiles-Air.attlocal.net>
2021-10-20 13:53:03 -0400

    restart and exist features

--- IV_wastingApps.py
+++ IV_wastingApps.py
@@ -6,10 +6,10 @@ from tkinter import messagebox, filedialog
 from tkmacosx import Button as button
 from tkcalendar import DateEntry
 import csv
+import sys
 import os
 
 
-WastedData = []
 
 # define command functions
 
@@ -26,8 +26,6 @@ def getrow(event):
 #Striped Treeview Rows with color
 
     
-    
-
 
 count = 1
 def update_wastedItemsList():
@@ -39,16 +37,17 @@ def update_wastedItemsList():
    if i % 2 == 0:
 
        trv.tag_configure("evenrow", background='lightgrey', foreground='black')
-
+       
        trv.insert(parent='', index='end' , text="", values= (count, ent2.get(), ent3.get(), ent4.get(), ent5.get()), tags=('evenrow',)) 
-    
+       
+       
    else:
 
        trv.tag_configure("oddrow", background='white', foreground='black')
-
+       
        trv.insert(parent='', index='end' , text="", values= (count, ent2.get(), ent3.get(), ent4.get(), ent5.get()), tags=('oddrow',)) 
     
-
+      
    count = count + 1
 
 
@@ -97,8 +96,28 @@ def ImportCSV():
 
 
 
+# Visualise the treeview contents
 
-# create root window
+
+def Treeview_VS():
+
+     for child in trv.get_children():
+
+      
+      
+
+          
+       
+
+          trV.tag_configure("oddrow", background='black', foreground='white')
+          
+          trV.insert("",'end',text=trv.item(child)["text"],values=trv.item(child)["values"], tags=('oddrow',))
+
+
+     
+
+# Initialize root as an object for Tk() class for creating a root window.
+
 root = Tk()
 
 
@@ -117,7 +136,7 @@ root.title("IV_WastingApps")
 
 # Specify the max size the window can exand to
 
-root.geometry("1430x750")
+root.geometry("1430x800")
 
 #Specify background color
 
@@ -127,16 +146,22 @@ root.config(bg="grey")
 
 wrapper = LabelFrame(root, width=700)
 wrapper.grid(row=0, column=0)
-wrapper3 = LabelFrame(root, width=700, height=760, text="Wasted IV_Meds Data Visualition", bg="lightgrey", font=("Helvetica", 15))
+wrapper3 = LabelFrame(root, width=700)
 wrapper3.grid(row=0, column=1)
 
-# Create wrappers and labels in left_frame
+#Create wrappers and labels in left_frame
 
-wrapper1 = LabelFrame(wrapper, width=700, height=450, text="IV_Meds wasted list", bg="lightgrey", font=("Helvetica", 15))
+wrapper1 = LabelFrame(wrapper, width=700, text="IV_Meds wasted list", bg="lightgrey", font=("Helvetica", 15))
 wrapper1.grid(row=0, column=0)
 wrapper2 = LabelFrame(wrapper, text="Wasting IV_Meds Portal", bg="lightgrey", font=("Helvetica", 15))
 wrapper2.grid(row=2, column=0)
 
+#Create wrappers and labels in right_frame
+
+wrapper4 = LabelFrame(wrapper3, width=700, text="Top five highest IV_Meds wasted", bg="lightgrey", font=("Helvetica", 15))
+wrapper4.grid(row=0, column=1)
+wrapper5 = LabelFrame(wrapper3, width=700, height=496,text="Bar Chart", bg="lightgrey", font=("Helvetica", 15))
+wrapper5.grid(row=2, column=1)
 
 # create treeview inside the IV_MedS Wasted list wrapper
 
@@ -147,16 +172,44 @@ vsb.pack(side ='right', fill ='y')
 trv.configure(yscrollcommand=vsb.set)
 style.configure("Treeview.Heading", font=("Helvetica", 14))
 
-#Configure treeview colors
+# create treeview inside the Top five highest IV_Meds wasted wrapper
+
+style = ttk.Style()
+trV = ttk.Treeview(wrapper4, columns=(1,2,3,4,5), show="headings", height=10)
+vsb = ttk.Scrollbar(wrapper4, orient="vertical", command=trV.yview)
+vsb.pack(side ='right', fill ='y')
+trV.configure(yscrollcommand=vsb.set)
+style.configure("Treeview.Heading", font=("Helvetica", 14))
+
+
+# create graph
+
+
+
+#Restart the application without closing the application windon
+
+def restart_Apps():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+
+
+
+#Configure left treeview colors
 
 style.configure("Treeview", background="lightblue", forgound="black", rowheight=25, fieldbackground="silver")
 trv.pack()
+
+#Configure right treeview colors
+
+style.configure("Treeview", background="lightblue", forgound="black", rowheight=25, fieldbackground="silver")
+trV.pack()
 
 #Change the color of selected row in treeview
 
 style.map('Treeview', background=[('selected', 'gray')])
 
-#Create striped row tags
+#Create striped row tags for left treeview
 
 trv.column("#1", anchor=CENTER, width=40)
 trv.column("#2", width=240)
@@ -171,8 +224,18 @@ trv.heading(4, text="Wasted Money $")
 trv.heading(5, text="Date")
 
 
+#Create striped row tags for right treeview
+trV.column("#1", anchor=CENTER, width=40)
+trV.column("#2", width=200)
+trV.column("#3", anchor=CENTER, width=140) 
+trV.column("#4", anchor=CENTER, width=140)
+trV.column("#5", anchor=CENTER, width=140)
 
-
+trV.heading(1, text="Index")
+trV.heading(2, text="Drug's Name")
+trV.heading(3, text="Wasted Number")
+trV.heading(4, text="Wasted Money $")
+trV.heading(5, text="Date")
 
 # create Wasting IV_Meds Portal
 
@@ -182,16 +245,16 @@ IV_Meds = [
     "gentamicin",
     "micafungin",
     "amphotericin",
-    "vancomycin",
-    "meropenem",
-    "gentamicin",
-    "micafungin",
-    "amphotericin",
-    "vancomycin",
-    "meropenem",
-    "gentamicin",
-    "micafungin",
-    "amphotericin"
+    "Cefazolin",
+    "Oxytocin",
+    "Insulin",
+    "Thiamine",
+    "Penicillin G",
+    "Cefoxitin",
+    "Cefepine",
+    "Zosyn",
+    "Mitronidazole",
+    "Meropenum"
 ]
 
 
@@ -226,18 +289,22 @@ Add_btn = button(wrapper2, text="Waste an Item", command=update_wastedItemsList,
 Export_toCSV = button(wrapper2, text="Export to CSV", command=Export_To_CSVFile, bg='lightblue')
 Remove_moreItem = button(wrapper2, text="Remove selected Items", command=Remove_moreItemFromList, bg='lightblue')
 ImportCVSfile = button(wrapper2, text="Import CSV File", command=ImportCSV, bg='lightblue')
+Trv_Visualise = button(wrapper2, text="Sort treeview", command=Treeview_VS, bg='lightblue')
+Exit_btn = button(wrapper2, text="Exit", command=root.quit, bg='white')
+Restart_btn = button(wrapper2, text="Restart", command=restart_Apps, bg='lightblue')
 
 Add_btn.grid(row=4, column= 0, padx=5, pady=10)
 Export_toCSV.grid(row=4, column= 1, padx=5, pady=10)
 Remove_moreItem.grid(row=5, column=0, padx=5, pady=3)
 ImportCVSfile.grid(row=5, column=1, padx=5, pady=10)
+Trv_Visualise.grid(row=6, column= 0, padx=5, pady=10)
+Restart_btn.grid(row=6, column= 1, padx=5, pady=10)
+Exit_btn.grid(row=7, columnspan=2)
 
 
+# create Wasting Bar Chart window
 
-
-
-
-
+lbl5 = Label(wrapper5, text=" Graph", height=3, bg="lightgrey", font=("Helvetica", 15))
 
 
 root.mainloop()
